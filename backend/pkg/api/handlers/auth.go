c859809cbbd2e6551ff3e8991f92457818389e60
Rob3a <arobaa23@gmail.com>
2025-05-08 00:53:32 +0300

    Add authentication handlers and middlewares

---
+++ backend/pkg/api/handlers/auth.go
@@ -0,0 +1,68 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/robaa12/mawid/pkg/services"
+)
+
+type AuthHandler struct {
+	AuthService *services.AuthService
+}
+
+func NewAuthHandler(authService *services.AuthService) *AuthHandler {
+	return &AuthHandler{
+		AuthService: authService,
+	}
+}
+
+func (h *AuthHandler) Register(c *gin.Context) {
+	var input services.RegisterInput
+
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error()})
+		return
+	}
+	response, err := h.AuthService.Register(input)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusCreated, gin.H{"data": response})
+}
+
+func (h *AuthHandler) Login(c *gin.Context) {
+	var input services.LoginInput
+
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	response, err := h.AuthService.Login(input)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, response)
+}
+
+func (h *AuthHandler) GetProfile(c *gin.Context) {
+	userID, exists := c.Get("user_id")
+	if !exists {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"error": "Unauthorized"})
+		return
+	}
+
+	user, err := h.AuthService.GetUserProfile(userID.(uint)) // convert userID from any to uint
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Failed to retrieve user profile"})
+		return
+	}
+}
---
+++ backend/pkg/api/middlewars/auth.go
@@ -0,0 +1,67 @@
+package middlewars
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/robaa12/mawid/config"
+	"github.com/robaa12/mawid/internal/utils"
+	"github.com/robaa12/mawid/pkg/models"
+)
+
+func AuthMidddleware(cfg *config.Config) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		// if there is no authHeader
+		if authHeader == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error": "Authorization header required"})
+			c.Abort()
+			return
+		}
+
+		// Extract token from bearer
+		tokenParts := strings.Split(authHeader, " ")
+		if len(tokenParts) != 2 || tokenParts[0] != "Bearer" {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error": "Invalid authorization format"})
+			c.Abort()
+			return
+		}
+
+		tokenString := tokenParts[1]
+		claims, err := utils.ValidateToken(tokenString, cfg)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error": "Invalid token"})
+			c.Abort()
+			return
+		}
+
+		c.Set("user_id", claims.UserID)
+		c.Set("email", claims.Email)
+		c.Set("role", claims.Role)
+		c.Next()
+	}
+}
+
+func AdminMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		role, exists := c.Get("role")
+		if !exists {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error": "Unauthorized"})
+			c.Abort()
+			return
+		}
+
+		if role != models.RoleAdmin {
+			c.JSON(http.StatusForbidden, gin.H{
+				"error": "Admin access required"})
+			c.Abort()
+			return
+		}
+		c.Next()
+	}
+}
