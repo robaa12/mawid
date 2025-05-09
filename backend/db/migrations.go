3cc173a1b35af9ef214420446aff5acfcf262c71
Rob3a <arobaa23@gmail.com>
2025-05-10 01:20:44 +0300

    fix: Add missing Tag model to AutoMigrate function

--- backend/db/migrations.go
+++ backend/db/migrations.go
@@ -10,7 +10,7 @@ import (
 func RunMigrations(db *gorm.DB) error {
 	log.Println("Running database migrations... ")
 
-	err := db.AutoMigrate(&models.User{}, &models.Category{}, &models.Event{}, &models.EventTag{})
+	err := db.AutoMigrate(&models.User{}, &models.Category{}, &models.Event{}, &models.EventTag{}, &models.Tag{})
 	if err != nil {
 		log.Printf("Migration failed: %v", err)
 		return err
